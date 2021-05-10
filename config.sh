e97ad9059d4ace60ed4e6aa04847bd5eb5718b38
trngkn <43349843+trngkn@users.noreply.github.com>
2021-05-10 22:57:34 +0700

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+read -p "Nhap router_id: " router_id
+opkg update
+# opkg install libustream-openssl
+opkg install nodogsplash
+
+wget --no-check-certificate https://bizfly-wifi.ss-hn-1.bizflycloud.vn/TP-Link/config/firewall
+wget --no-check-certificate https://bizfly-wifi.ss-hn-1.bizflycloud.vn/TP-Link/config/network
+wget --no-check-certificate https://bizfly-wifi.ss-hn-1.bizflycloud.vn/TP-Link/config/login.sh
+wget --no-check-certificate https://bizfly-wifi.ss-hn-1.bizflycloud.vn/TP-Link/config/nodogsplash
+wget --no-check-certificate https://bizfly-wifi.ss-hn-1.bizflycloud.vn/TP-Link/config/wireless
+sed -i "s/id123123/$router_id/g" login.sh
+chmod +x login.sh
+
+cp -v network firewall nodogsplash wireless /etc/config/
+cp  -v login.sh /usr/lib/nodogsplash/
+/etc/init.d/nodogsplash restart
+
+echo "SUCCESS!"
