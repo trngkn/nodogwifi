fe2c2d38826879a1be01da798d9453fe763eb9c0
trngkn <43349843+trngkn@users.noreply.github.com>
2021-05-10 22:59:49 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,8 +6,8 @@ opkg update
 opkg install nodogsplash
 
 wget --no-check-certificate https://bizfly-wifi.ss-hn-1.bizflycloud.vn/TP-Link/config/firewall
-wget --no-check-certificate https://bizfly-wifi.ss-hn-1.bizflycloud.vn/TP-Link/config/network
-wget --no-check-certificate https://bizfly-wifi.ss-hn-1.bizflycloud.vn/TP-Link/config/login.sh
+wget --no-check-certificate https://raw.githubusercontent.com/trngkn/nodogwifi/main/network
+wget --no-check-certificate https://raw.githubusercontent.com/trngkn/nodogwifi/main/login.sh
 wget --no-check-certificate https://bizfly-wifi.ss-hn-1.bizflycloud.vn/TP-Link/config/nodogsplash
 wget --no-check-certificate https://bizfly-wifi.ss-hn-1.bizflycloud.vn/TP-Link/config/wireless
 sed -i "s/id123123/$router_id/g" login.sh
