8fb36c35a3f69d43f5582c8fdcf8e53d52eb2750
trngkn <43349843+trngkn@users.noreply.github.com>
2021-05-11 21:34:40 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,11 +4,11 @@ read -p "Nhap router_id: " router_id
 opkg install libustream-openssl
 opkg install nodogsplash
 
-wget --no-check-certificate https://bizfly-wifi.ss-hn-1.bizflycloud.vn/TP-Link/config/firewall
+wget --no-check-certificate https://raw.githubusercontent.com/trngkn/nodogwifi/main/firewall
 wget --no-check-certificate https://raw.githubusercontent.com/trngkn/nodogwifi/main/network
-wget --no-check-certificate https://bizfly-wifi.ss-hn-1.bizflycloud.vn/TP-Link/config/login.sh
-wget --no-check-certificate https://bizfly-wifi.ss-hn-1.bizflycloud.vn/TP-Link/config/nodogsplash
-wget --no-check-certificate https://bizfly-wifi.ss-hn-1.bizflycloud.vn/TP-Link/config/wireless
+wget --no-check-certificate https://raw.githubusercontent.com/trngkn/nodogwifi/main/login.sh
+wget --no-check-certificate https://raw.githubusercontent.com/trngkn/nodogwifi/main/nodogsplash
+wget --no-check-certificate https://raw.githubusercontent.com/trngkn/nodogwifi/main/wireless
 sed -i "s/id123123/$router_id/g" login.sh
 chmod +x login.sh
 
